61c513cf9f81b5c69d2b098c92c2293360ef8624
Dhirendra <dhirendrabhoi40@gmail.com>
2026-07-29 16:19:13 +0530

    initialize Express server with MongoDB connection and contact routes

--- Backend/server.js
+++ Backend/server.js
@@ -10,11 +10,12 @@ const app = express();
 // const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:5173', // Vite dev server
-  methods: ['GET', 'POST'],
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'http://localhost:5173', // Vite dev server
+//   methods: ['GET', 'POST'],
+//   credentials: true,
+// }));
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -40,3 +41,5 @@ const connectDB = async () => {
 //     console.log(`🚀 Server running at http://localhost:${PORT}`);
 //   });
 // });
+
+export default app;
